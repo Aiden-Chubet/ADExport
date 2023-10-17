0afc83c7e198e57ac0ee76330f3c39e30a15a7ae
ach-u-be14 <147650530+ach-u-be14@users.noreply.github.com>
2023-10-17 14:13:47 -0400

    Script

---
+++ ADexport.ps1
@@ -0,0 +1,3 @@
+﻿Get-ADUser -Filter * -SearchScope OneLevel -SearchBase "OU=Employees,DC=MYDOMAIN,DC=local" -Properties * |
+    Select-Object Displayname,Department,Mobile |
+    export-csv -path C:\writable\userexportnew.csv
